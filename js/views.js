36d794363e4067609eb484582f9be0f7bcab211b
Van Nguyen <vanleslienguyen@gmail.com>
2016-05-11 20:16:09 -0700

    cleaned up the code

--- js/views.js
+++ js/views.js
@@ -1,69 +1,68 @@
 //different page values
-
 //home screen to host game
 var homescreen = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\"> Come play a fun game of fill in the blank! </p>"+
-			"<button onclick=\"hostGame()\" id=\"hostButton\" class=\"btn btn-lg btn-success\" role=\"button\">Host a Game</button>" +
-		    "</div>";
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\"> Come play a fun game of fill in the blank! </p>" +
+    "<button onclick=\"hostGame()\" id=\"hostButton\" class=\"btn btn-lg btn-success\" role=\"button\">Host a Game</button>" +
+    "</div>";
 
 //set up screen to start game
 var setupscreen = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\"> Come play a fun game of fill in the blank! </p>"+
-			"</br> Wait for some friends and then click start!</br>" +
-			"<button onclick=\"startTheGame()\" id=\"startButton\" class=\"btn btn-lg btn-success\" role=\"button\">Start the Game</button>" +
-		    "</div>";
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\"> Come play a fun game of fill in the blank! </p>" +
+    "</br> Wait for some friends and then click start!</br>" +
+    "<button onclick=\"startTheGame()\" id=\"startButton\" class=\"btn btn-lg btn-success\" role=\"button\">Start the Game</button>" +
+    "</div>";
 
 //set up waiting screen
 var waitingscreen = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\"> Waiting for the host to start the game...</p>"+
-			"</br> Just wait it'll be fun!" +
-		    "</div>";
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\"> Waiting for the host to start the game...</p>" +
+    "</br> Just wait it'll be fun!" +
+    "</div>";
 
 //screen after host disconnects
 var resethomescreen = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\">" + 
-			"Host disconnected! Oh noo. It's ok. Let's just pretend that didn't happen and try again.</br></br>" +
-			"Come play a fun game of fill in the blank! </p>"+
-			"<button onclick=\"hostGame()\" id=\"hostButton\" class=\"btn btn-lg btn-success\" role=\"button\">Host a Game</button>" +
-		    "</div>";
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\">" +
+    "Host disconnected! Oh noo. It's ok. Let's just pretend that didn't happen and try again.</br></br>" +
+    "Come play a fun game of fill in the blank! </p>" +
+    "<button onclick=\"hostGame()\" id=\"hostButton\" class=\"btn btn-lg btn-success\" role=\"button\">Host a Game</button>" +
+    "</div>";
 
 //set up waiting screen after story submission
 var storysubmitwaitingscreen = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\"> Waiting for everyone to finish their story</p>"+
-			"</br> Don't worry it won't be as funny" +
-		    "</div>";		   
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\"> Waiting for everyone to finish their story</p>" +
+    "</br> Don't worry it won't be as funny" +
+    "</div>";
 
 //set up waiting screen after story submission
 var showstoryheader = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\"> Here it is!</p>"+
-			"Laugh it up!" +
-		    "</div>";
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\"> Here it is!</p>" +
+    "Laugh it up!" +
+    "</div>";
 
 //about
 var about = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\"> About</p>"+
-			"Mad Libs is a fun game that can be played by anyone. It takes your input and creates stories for everyone to read." +
-		    "</div>";		    		     		    
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\"> About</p>" +
+    "Mad Libs is a fun game that can be played by anyone. It takes your input and creates stories for everyone to read." +
+    "</div>";
 
 //how to play
 var howtoplay = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\">How to Play</p>"+
-			"Wait for a host or host your own! Once the game starts, fill in the blanks" 
-			+ " with the requested input and then hit submit. Once all of" + 
-			" the players have entered their words, your story and theirs will be sent back to you to read! You can play with as little or as many people as you want." +
-		    "</div>";		    
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\">How to Play</p>" +
+    "Wait for a host or host your own! Once the game starts, fill in the blanks" +
+    " with the requested input and then hit submit. Once all of" +
+    " the players have entered their words, your story and theirs will be sent back to you to read! You can play with as little or as many people as you want." +
+    "</div>";
 
 //about
 var contact = "<div class=\"jumbotron\" id=\"theJumbotron\">" +
-			"<h1>Mad Libs</h1>" + 
-			"<p class=\"lead\"> Contact Us</p>"+
-			"Write down your thoughts, fold it into a paper airplane and toss it into the sky. Once we receive it, we'll be sure to read it!" +
-		    "</div>";
+    "<h1>Mad Libs</h1>" +
+    "<p class=\"lead\"> Contact Us</p>" +
+    "Write down your thoughts, fold it into a paper airplane and toss it into the sky. Once we receive it, we'll be sure to read it!" +
+    "</div>";
--- server.js
+++ server.js
@@ -13,7 +13,9 @@ var bodyParser = require("body-parser");
 app.use(express.static(__dirname));
 
 //body parser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 
@@ -37,7 +39,7 @@ MongoClient.connect(url, function(err, db) {
     console.log("in monogo connect");
     console.log("Mongo Connection Error: " + err);
     findStory(db, function(doc) {
-        if (doc.length === 0){
+        if (doc.length === 0) {
             insertStory(db, function() {
                 db.close();
             });
@@ -45,30 +47,20 @@ MongoClient.connect(url, function(err, db) {
     });
 });
 console.log("outside mongoconnect");
-//gets stories from json file
-/*var storycollection [];
-var jsonStory = "stories.json";
-    fs.readFile(jsonStory, function(err, data) {
-        if(err) {
-            console.log(err);
-        } else {
-            var jsondata = JSON.parse(data);
-            storycollection = jsondata.story;
-        }
-    })*/
 
 //Inserts sample mad lib story
 var insertStory = function(db, callback) {
     var collection = db.collection('stories');
     collection.insert(
-        [
-            {story:"Say cheese the photographer said as the camera flashed! and I had gone to to get our photos taken today. The first photo we really wanted was a picture of us dressed as [[Animals]] pretending to be [[Feeling]]. When we saw the proofs of it, I was a bit [[Things (Plural)]] because it looked different than in my head. (I hadn't imagined so many [[A Professional (like 'Baker')]] behind us.)"},
-            {story:"A vacation is when you take a trip to some [[Adjective]] place with your [[Adjective (2nd)]] family. Usually you go to some place that is near a/an [[Noun]] or up on a/an [[Noun (2nd)]]. A good vacation place is one where you can ride [[Animal (Plural)]] or play [[Game]]."}
-        ],
+        [{
+            story: "Say cheese the photographer said as the camera flashed! and I had gone to to get our photos taken today. The first photo we really wanted was a picture of us dressed as [[Animals]] pretending to be [[Feeling]]. When we saw the proofs of it, I was a bit [[Things (Plural)]] because it looked different than in my head. (I hadn't imagined so many [[A Professional (like 'Baker')]] behind us.)"
+        }, {
+            story: "A vacation is when you take a trip to some [[Adjective]] place with your [[Adjective (2nd)]] family. Usually you go to some place that is near a/an [[Noun]] or up on a/an [[Noun (2nd)]]. A good vacation place is one where you can ride [[Animal (Plural)]] or play [[Game]]."
+        }],
         function(err, result) {
             console.log("Mongo Insertion Error:" + err);
             callback(result);
-    });
+        });
 };
 
 //Find random mad lib story from the database to use
@@ -90,9 +82,11 @@ var findStoryByID = function(id, db, callback) {
     var collection = db.collection('stories');
 
     //convert string to mongodb object
-    var o_id = new mongo.ObjectID(id); 
+    var o_id = new mongo.ObjectID(id);
 
-    collection.findOne({'_id': o_id}, function(err, doc) {
+    collection.findOne({
+        '_id': o_id
+    }, function(err, doc) {
         console.log("Found the following record");
         console.dir(doc);
         callback(doc);
@@ -115,7 +109,7 @@ var hostname = "";
 var storyArray = {
     length: 0,
 
-    addElem: function addElem (elem) {
+    addElem: function addElem(elem) {
         // obj.length is automatically incremented every time an element is added.
         [].push.call(this, elem);
     }
@@ -132,10 +126,10 @@ var intervals = [];
 
 
 //when a player connects; socket starts here
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function(socket) {
     console.log("user connected");
     var player = new playerDef(socket.id);
-    
+
     playersArray.push(player);
 
     console.log(player);
@@ -146,7 +140,7 @@ io.sockets.on('connection', function(socket){
     socket.emit('reload lobby');
 
     // remove player from game on disconnect
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function() {
         console.log("player disconnected");
         playersArray.length = 0;
         stories.length = 0;
@@ -172,18 +166,18 @@ io.sockets.on('connection', function(socket){
         //create game
         game = new gameDef(host.getId());
         gamestatus = "initializing";
-        
+
         //send msg to other players
         io.to('gameroom').emit('waitingforhost');
         //var hostinroom = socket.clients('host');
         //var playersinroom = socket.clients('gameroom');
         //console.log(hostinroom);
         //console.log(playersinroom);
-        
+
     })
 
     //start game
-    socket.on('startgame', function(){
+    socket.on('startgame', function() {
         console.log("in socket on of start game");
         io.to('host').emit('showform1');
         io.to('gameroom').emit('showform2');
@@ -191,21 +185,23 @@ io.sockets.on('connection', function(socket){
     })
 
     //grab and hold story
-    socket.on('waitforothers', function(data){
+    socket.on('waitforothers', function(data) {
         console.log("in waitforothers");
-        
+
         //IS 2016-05-09: append array only if data is defined
         if (typeof data != 'undefined') {
             //add story to array
-            var tempstory=data.story;
+            var tempstory = data.story;
             //attempt to make a json array to pass
-            storyArray.addElem({story: tempstory});
+            storyArray.addElem({
+                story: tempstory
+            });
             //pushing to the regular stories array defined as []
             stories.push(tempstory);
         }
 
         //if all stories are submitted, show them
-        if(playersArray.length == stories.length){
+        if (playersArray.length == stories.length) {
             //show stories
             console.log("in show stories");
             //adjust this to pass the json object
@@ -213,25 +209,24 @@ io.sockets.on('connection', function(socket){
 
             socket.emit('showstory', stories);
 
-        }
-        else{
+        } else {
             console.log("in nothing of waitforothers");
             console.log("#players: " + playersArray.length + " story length: " + stories.length);
-            console.log(playersArray.length===stories.length);
+            console.log(playersArray.length === stories.length);
             //do nothing
             //socket.emit('waitstoryscreen');
 
             //IS 2016-05-09 - checking for other players' submission - 5 seconds
             var inputInterval = setInterval(function() {
                 socket.emit('checkOtherPlayerInput');
-            },5000);
+            }, 5000);
 
             intervals.push(inputInterval);
         }
     })
 
-    socket.on('terminateSetInterval', function () {
-         //IS 2016-05-09: break from inputInterval if it's defined
+    socket.on('terminateSetInterval', function() {
+        //IS 2016-05-09: break from inputInterval if it's defined
         if (intervals != null) {
             intervals.forEach(clearInterval);
             //clearInterval(inputInterval);
@@ -245,15 +240,15 @@ var fixedInt;
 
 //Return story and stories id
 app.get("/getStory", function(req, res) {
-    if (typeof req.query.randomNum != 'undefined' && req.query.randomNum != 7 ) {
+    if (typeof req.query.randomNum != 'undefined' && req.query.randomNum != 7) {
         console.log('DEBUG - the random number is = ' + req.query.randomNum);
         fixedInt = req.query.randomNum;
     }
     MongoClient.connect(url, function(err, db) {
         findStory(db, function(doc) {
-            
+
             console.log("Fixed integer = " + fixedInt);
-           
+
             res.json({
                 story: doc[fixedInt].story,
                 id: doc[fixedInt]._id
@@ -283,18 +278,18 @@ app.get("/getStoryById", function(req, res) {
 app.post("/createNewGame", function(req, res) {
     MongoClient.connect(url, function(err, db) {
         var collection = db.collection('games');
-        
+
         // Look for random story Id in the DB
         // Doesn't do that yet, hard-coded the story id for now
         var newStoryId = "5709d7f045e3cbfb09d22164";
 
-        collection.insertOne(
-            {storyId: newStoryId,
-             isPlayer1Ready: true,
-             isPlayer2Ready: false,
-             player1Story: "",
-             player2Story: "" 
-         });
+        collection.insertOne({
+            storyId: newStoryId,
+            isPlayer1Ready: true,
+            isPlayer2Ready: false,
+            player1Story: "",
+            player2Story: ""
+        });
         db.close();
     });
 });
@@ -305,15 +300,15 @@ app.get("/joinGame", function(req, res) {
     console.log("/joinGame");
     MongoClient.connect(url, function(err, db) {
         var collection = db.collection('games');
-        collection.findOne({"isPlayer2Ready":false}, function(err, doc) {
+        collection.findOne({
+            "isPlayer2Ready": false
+        }, function(err, doc) {
             if (err) {
                 console.log("ERROR = " + err);
-            }
-            else if (doc) {
+            } else if (doc) {
                 console.log('Found: ' + doc);
                 res.json(doc);
-            } 
-            else {
+            } else {
                 console.log('no data yet!')
                 res.json(doc);
             }
@@ -325,33 +320,36 @@ app.get("/joinGame", function(req, res) {
 
 
 // get the input value and stories id and put it in the DB
-app.post("/inputs", function(req, res) { 
+app.post("/inputs", function(req, res) {
     MongoClient.connect(url, function(err, db) {
         console.log("post Error: " + err);
         console.log("req.body", req.body);
-        console.log("req.body.id:  "+req.body.id  );
+        console.log("req.body.id:  " + req.body.id);
         var inputsObj = req.body;
-        insertInputValue(db, inputsObj, function() { db.close(); }); 
+        insertInputValue(db, inputsObj, function() {
+            db.close();
+        });
         res.end("OK");
     });
-});//end post
+}); //end post
 
 var insertInputValue = function(db, inputsObj, callback) {
     var collection = db.collection('inputs');
-    collection.insertOne(
-        { input: inputsObj },
+    collection.insertOne({
+            input: inputsObj
+        },
         function(err, result) {
             console.log("Insert input values Error:" + err);
             console.log("Insert input values result:" + result);
             callback(result);
-    });
+        });
 };
- 
+
 //Return inputs and id
 app.get("/inputs", function(req, res) {
     MongoClient.connect(url, function(err, db) {
-         console.log("app.get req.body.id:  "+req.body.id);
-          findInput(db, function(doc) {
+        console.log("app.get req.body.id:  " + req.body.id);
+        findInput(db, function(doc) {
             res.json({
                 inputs: doc
             });
